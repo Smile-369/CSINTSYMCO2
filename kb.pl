45561390d0136714553b4f30dca8c1522f1d6f76
cedyoung <160330275+cedyoung@users.noreply.github.com>
2024-11-27 16:52:05 +0800

    Update kb.pl

--- kb.pl
+++ kb.pl
@@ -4,9 +4,14 @@
 :- dynamic parent/2.
 :- dynamic sibling/2.
 
+
+
 % Rules for family relationships
-father(X, Y) :- parent(X, Y), male(X).
-mother(X, Y) :- parent(X, Y), female(X).
+father(X, Y) :- parent(X, Y), male(X),
+     X \= Y.
+
+mother(X, Y) :- parent(X, Y), female(X), 
+    X\= Y.
 
 sibling(X, Y) :- 
     parent(Z, X), parent(Z, Y), 
@@ -15,8 +20,8 @@ sibling(X, Y) :-
     Z \= T, 
     X \= Y.
 
-brother(X, Y) :- sibling(X, Y), male(X).
-sister(X, Y) :- sibling(X, Y), female(X).
+brother(X, Y) :- sibling(X, Y), male(X), X \= Y.
+sister(X, Y) :- sibling(X, Y), female(X), X \= Y.
 
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
 grandchild(X, Y) :- grandparent(Y, X).
@@ -26,6 +31,8 @@ aunt(X, Y) :- parent(Z, Y), sibling(X, Z), female(X).
 
 nephew(X, Y) :- sibling(Z, Y), parent(Z, X), male(X).
 niece(X, Y) :- sibling(Z, Y), parent(Z, X), female(X).
+
+
 
 % Validation rule: prevent contradictions
 impossible :- fail.
